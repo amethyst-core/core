84fcd2e92ed7b54ff10d93a13dde4ec6b95dc19e
eveeifyeve <eveeg1971@gmail.com>
2024-05-24 17:07:36 +1000

    feat: simple api setup 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+mod handler;
+mod routes;
+
+use axum::http::header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE};
+use axum::{http::header::HeaderValue, http::Method};
+use routes::create_router;
+use tower_http::cors::CorsLayer;
+
+#[tokio::main]
+async fn main() {
+    let cors = CorsLayer::new()
+        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
+        .allow_credentials(true)
+        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
+
+    let app = create_router().layer(cors);
+
+    println!("🔮 Server has successfully started");
+    let listner = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    axum::serve(listner, app).await.unwrap();
 }
